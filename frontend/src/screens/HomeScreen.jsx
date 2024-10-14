8aaf92a1d60624a71a03fd20945d2d27fdddca15
naonao-aaa <naonao1814naonao@gmail.com>
2024-10-14 21:49:56 +0900

    バッグエンドの商品データを取得して表示させる。 npm i axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
-import products from "../products";
+import Product from "../components/Product"; // Productコンポーネントをインポート
+import axios from "axios"; // HTTPリクエストを行うためにaxiosをインポート
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]); // productsのStateを管理。初期値は空の配列
+
+  // useEffectフック：コンポーネントが最初にレンダリングされたときに一度だけ実行される
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products`); // データを取得（/api/products エンドポイントにGETリクエスト）
+      setProducts(data); // 取得したデータを、productsのstateにセット
+    };
+
+    fetchProducts(); // fetchProducts関数を呼び出して商品データを取得
+  }, []);
+
   return (
     <>
       <h1>商品一覧</h1>
