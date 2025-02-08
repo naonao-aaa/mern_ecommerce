2f848528a7ba5b048dcd74dc12c6d4ecf9689374
naonao-aaa <naonao1814naonao@gmail.com>
2025-02-08 18:04:20 +0900

    (feat)pagination

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,19 @@
 import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import Product from "../components/Product"; // Productコンポーネントをインポート
 import { useGetProductsQuery } from "../slices/productsApiSlice"; // RTK QueryのuseGetProductsQueryフックをインポート
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery(); // 商品データを取得
+  // URLパラメータからページ番号を取得
+  const { pageNumber } = useParams();
+  console.log(pageNumber);
+
+  // 商品データを取得
+  const { data, isLoading, error } = useGetProductsQuery({
+    pageNumber, // ページ番号をパラメータとして渡す
+  });
 
   return (
     <>
@@ -19,7 +27,7 @@ const HomeScreen = () => {
         <>
           <h1>商品一覧</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
