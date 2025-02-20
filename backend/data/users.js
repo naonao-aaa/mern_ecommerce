77da1f8b689aab0300edc1cd2c0890d6823ae392
naonao-aaa <naonao1814naonao@gmail.com>
2025-02-20 19:38:57 +0900

    (refactor)パスワードを環境変数に移行など

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,21 +1,24 @@
 import bcrypt from "bcryptjs"; // bcryptjsモジュールをインポート（パスワードのハッシュ化に使用）
+import dotenv from "dotenv"; // dotenvモジュールをインポート（環境変数を読み込むため）
+
+dotenv.config(); // .envファイルに記載された環境変数を読み込んで、process.envに設定
 
 const users = [
   {
     name: "Admin User",
     email: "admin@email.com",
-    password: bcrypt.hashSync("123456", 10), // パスワードをハッシュ化
+    password: bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10), // パスワードをハッシュ化
     isAdmin: true, // 管理者フラグをtrueに設定
   },
   {
     name: "澤部佑",
     email: "sawabe@email.com",
-    password: bcrypt.hashSync("123456", 10), // パスワードをハッシュ化
+    password: bcrypt.hashSync(process.env.SAWABE_PASSWORD, 10), // パスワードをハッシュ化
   },
   {
     name: "若林正恭",
     email: "wakabayashi@email.com",
-    password: bcrypt.hashSync("123456", 10), // パスワードをハッシュ化
+    password: bcrypt.hashSync(process.env.WAKABAYASHI_PASSWORD, 10), // パスワードをハッシュ化
   },
 ];
 
